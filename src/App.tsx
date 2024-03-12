689cbd170703bc7b0a26c68de51c7b79cb78316c
tomy08 <tomasesantacruz@gmail.com>
2024-03-11 22:12:57 -0300

    change component name

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { JSX } from "react";
+import { JSX } from 'react'
 
-import Header from "./components/Header";
-import Nav from "./components/Nav";
+import Header from './components/Header'
+import Main from './components/Nav'
 
 export default function App(): JSX.Element {
   return (
     <>
       <Header />
-      <Nav />
+      <Main />
     </>
-  );
+  )
 }
